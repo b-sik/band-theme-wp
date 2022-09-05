1fcd8bd7ef68f43a9c0aa920c1b61ebf7bd0957f
Brian Siklinski <bszyk.codes@gmail.com>
2022-09-05 15:44:43 -0400

    hide nav and put icons in featured music

--- parts/shared/featured-music.php
+++ parts/shared/featured-music.php
@@ -1,7 +1,27 @@
+<?php
+$home = get_page_by_title( 'home' );
+
+$streaming_services = get_field( 'streaming_services', $home->ID );
+$socials            = get_field( 'social_media', $home->ID );
+
+$icons = array_merge( $streaming_services, $socials );
+?>
+
 <section id="featured-music" class="container-fluid bg-dark py-5 vh-50">
-	<div class="row my-auto">
+	<div class="row my-auto py-5">
 		<div class="col-10 offset-1 d-flex justify-content-center my-3">
 		<iframe style="border: 0; width: 400px; height: 307px;" src="https://bandcamp.com/EmbeddedPlayer/album=834360237/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="https://westferry.bandcamp.com/album/out-of-reach">Out of Reach by west ferry</a></iframe>
 		</div>
 	</div>
+	<div class="row justify-content-center align-items-center">
+			<?php
+			foreach ( $icons as $icon ) :
+				$i_title = $icon['title'];
+				$i_url   = $icon['url'];
+				?>
+				<a class="icon-anchor ms-2 p-0 text-white" href="<?php echo esc_attr( $i_url ); ?>" target="_blank" rel="noopener noreferrer" style="width:50px;height:50px;">
+					<i class="fa-brands fa-<?php echo esc_attr( strtolower( $i_title ) ); ?> h-100 w-100"></i>
+				</a>
+			<?php endforeach; ?>
+		</div>
 </section>
--- parts/shared/header.php
+++ parts/shared/header.php
@@ -25,14 +25,10 @@ $menu_args_mobile = array(
 
 $band_name          = get_field( 'band_name', $home->ID );
 $featured_img_url   = get_the_post_thumbnail_url( $home->ID );
-$streaming_services = get_field( 'streaming_services', $home->ID );
-$socials            = get_field( 'social_media', $home->ID );
-
-$icons = array_merge( $streaming_services, $socials );
 ?>
 
 <header id="header" class="vh-100 d-flex flex-column position-relative">
-	<nav class="navbar navbar-expand-lg navbar-dark bg-info bg-gradient px-2 py-1 py-lg-0 position-fixed w-100">
+	<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-info bg-gradient px-2 py-1 py-lg-0 position-fixed w-100">
 		<div class="row justify-content-apart px-0 mx-0 w-100">
 			<div class="col-9 col-lg-3 d-flex align-items-center ps-0">
 				<a href="#" class="h1 brand"><?php echo esc_html( $band_name ); ?></a>
@@ -68,13 +64,17 @@ $icons = array_merge( $streaming_services, $socials );
 				<?php wp_nav_menu( $menu_args_mobile ); ?>
 			</div>
 		</div>
-	</nav>
+	</nav> -->
 	<section class="container-fluid hero flex-grow-1 mb-3 px-0"
 		style="background-image:url(<?php echo esc_attr( $featured_img_url ); ?>);background-position:center;background-size:cover;">
 	</section>
 	<div id="hero-content-overlay" class="container-fluid min-vh-100 w-100 position-absolute px-2" style="z-index:1033;">
-		<div class="col-12 min-vh-100 w-100 d-flex flex-column justify-content-end text-center text-lg-start px-0">
-			<div style="color: #fff;text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">
+		<div class="col-12 min-vh-100 w-100 d-flex flex-column justify-content-between text-center text-lg-start px-0">
+			<div class="text-outline d-flex">
+				<h1 style="color:#c270d6;"><em>west ferry</em></h1>
+			
+			</div>
+			<div class="text-outline" style="color:#fff;">
 				<h1>NEW EP 'OUT OF REACH'</h1>
 				<h2>available now</h2>
 			</div>
