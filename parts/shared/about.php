14b79bf301dcb9f29dd0e7591da74b18edce39b6
Brian Siklinski <bszyk.codes@gmail.com>
2022-09-05 19:39:10 -0400

    about header

--- parts/shared/about.php
+++ parts/shared/about.php
@@ -4,8 +4,8 @@ $page = get_page_by_title( 'About' );
 
 <section id="about" class="container-fluid bg-dark about-wrapper py-5 min-vh-100">
 	<div class="row align-items-center h-100">
-		<div class="col-10 offset-1 py-3">
-			<h3><?php echo __( 'About', 'westferry' ); ?></h3>
+		<div class="col-10 col-lg-8 offset-1 offset-lg-2 pt-3 mb-3 text-center border-bottom">
+			<h3><?php echo __( 'About the Band', 'westferry' ); ?></h3>
 		</div>
 
 		<div class="col-10 offset-1 col-lg-8 offset-lg-2">
