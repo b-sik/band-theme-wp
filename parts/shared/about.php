4de41ac696bd14cc6786d778e228cf1b01cde4be
Brian Siklinski <bszyk.codes@gmail.com>
2022-09-05 18:38:11 -0400

    mostly footer/contact layout work

--- parts/shared/about.php
+++ parts/shared/about.php
@@ -2,11 +2,13 @@
 $page = get_page_by_title( 'About' );
 ?>
 
-<section id="about" class="container-fluid bg-dark about-wrapper py-5 gx-5">
-	<h3><?php echo __( 'About', 'westferry' ); ?></h3>
+<section id="about" class="container-fluid bg-dark about-wrapper py-5 min-vh-100">
+	<div class="row align-items-center h-100">
+		<div class="col-10 offset-1 py-3">
+			<h3><?php echo __( 'About', 'westferry' ); ?></h3>
+		</div>
 
-	<div class="row justify-content-center">
-		<div class="col-12">
+		<div class="col-10 offset-1 col-lg-8 offset-lg-2">
 			<?php echo apply_filters( 'the_content', $page->post_content ); ?>
 		</div>
 	</div>
--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -17,40 +17,37 @@ $icons = array_merge( $streaming_services, $socials );
 
 <footer class="container-fluid justify-content-center vh-100 d-flex align-items-end flex-column mx-0 px-0" style="background-image: linear-gradient(to top, #0e0e2600 25%, #0e0e26ff), url(<?php echo esc_attr( $contact_featured_img_url ); ?>);background-position:center bottom;background-size:cover;background-repeat:no-repeat;">
 
-	<section id="contact" class="container contact-wrapper flex-grow-1 d-flex flex-column justify-content-center">
-		<h3 class="text-center"><?php echo __( 'Contact', 'westferry' ); ?></h3>
-		<div class="row">
-			<div class="col-12 d-flex justify-content-center">
-				<a class="text-center btn btn-primary" href="mailto:<?php echo $email; ?>">
+	<section id="contact" class="container-fluid contact-wrapper flex-grow-1 d-flex flex-column justify-content-center">
+		<div class="row bg-dark py-5" style="--bs-bg-opacity: .9;">
+			<div class="col-12 d-flex justify-content-center w-100" >
+				<a class="text-center email-anchor" href="mailto:<?php echo $email; ?>">
 					<span class="d-inline-block h1"><?php echo $email_exploded[0]; ?></span><span class="d-inline-block h1">&commat;<?php echo $email_exploded[1]; ?></span>
 				</a>
 			</div>
-		</div>
-	</section>
-
-	<div class="row py-2 mx-0 flex-grow-0 w-100">
-		<div
-			class="col-12 col-lg-5 d-flex flex-column align-items-center align-items-lg-start justify-content-center order-2 order-lg-0">
-			<small class='d-block'>&copy;&nbsp;<?php echo date( 'Y' ); ?></small>
-			<small class='d-block'><?php echo $footer_text; ?></small>
-		</div>
-		<div class="col-12 col-lg-2 d-flex justify-content-center align-items center">
-			<img src="<?php echo $icon_logo; ?>" class="img-fluid w-50 mb-3 mb-lg-0" style="max-width:100px;height:auto;">
-		</div>
-		<div class="col-12 col-lg-5 pb-3 pb-lg-0 d-flex justify-self-lg-end align-items-center">
-			<div class="container d-flex justify-content-center justify-content-lg-end">
-				<div class="row">
+			<div class="row justify-content-center pt-3">
 				<?php
 				foreach ( $icons as $icon ) :
 					$i_title = $icon['title'];
 					$i_url   = $icon['url'];
 					?>
-					<a class="icon-anchor ms-2 p-0" href="<?php echo esc_attr( $i_url ); ?>" target="_blank" rel="noopener noreferrer" style="width:40px;height:40px;">
+					<a class="icon-anchor ms-2 p-0" href="<?php echo esc_attr( $i_url ); ?>" target="_blank" rel="noopener noreferrer" style="width:60px;height:60px;">
 						<i class="fa-brands fa-<?php echo esc_attr( strtolower( $i_title ) ); ?> h-100 w-100"></i>
 					</a>
 				<?php endforeach; ?>
-				</div>
 			</div>
 		</div>
+
+	</section>
+
+	<div class="row py-2 mx-0 w-100">
+		<div
+			class="col-12 d-flex flex-column align-items-center align-items-lg-start justify-content-center order-2 order-lg-0">
+			<small class='d-block'>
+			<!-- &copy;&nbsp;<?php echo date( 'Y' ); ?> |  -->
+			Made with 🖤 by <a href="https://bsik.dev" target="_blank" rel="noopener noreferrer">bsik.dev</a></small>
+		</div>
+		<!-- <div class="col-12 col-lg-2 d-flex justify-content-center align-items center">
+			<img src="<?php echo $icon_logo; ?>" class="img-fluid w-50 mb-3 mb-lg-0" style="max-width:100px;height:auto;">
+		</div> -->
 	</div>
 </footer>
