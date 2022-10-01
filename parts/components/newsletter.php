2ad5208848d15e11b6ebe418f6865f42932e8095
Brian Siklinski <bszyk.codes@gmail.com>
2022-10-01 19:24:38 -0400

    newsletter input styling tweak

--- parts/components/newsletter.php
+++ parts/components/newsletter.php
@@ -1,8 +1,9 @@
 <div class="newsletter-wrapper container-fluid pt-5">
 	<div class="row">
 		<div class="col-10 col-lg-8 offset-1 offset-lg-2">
+			<p class="text-center w-100">Sign up for our newsletter and never miss a show&nbsp;&darr;</p>
         <?php //phpcs:ignore
-        if ( function_exists( 'newsletters_hardcode' ) ) {
+		if ( function_exists( 'newsletters_hardcode' ) ) {
 			newsletters_hardcode( false, false, false, 1 );
 		}
 		?>
